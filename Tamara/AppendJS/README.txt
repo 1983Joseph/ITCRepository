9915351de4e7ef079beffb8f0cfd9f9fb1861a3a
student <you@example.com>
2014-05-20 23:45:51 +0400

    appending items js

---
+++ Tamara/AppendJS/README.txt
@@ -0,0 +1,16 @@
+This is a README file for my web page.
+
+Content
+1.Author
+2.Introduction
+
+Author
+ITC
+Tamara Avagyan
+
+Introduction
+
+This is a program, which append items from textbox into list end array. There are two buttons` ADD and ShowArray. 
+Button ADD append items into list and array.
+Button ShowArray show us array content.
+
---
+++ Tamara/AppendJS/js/list.js
@@ -0,0 +1,26 @@
+var a=new Array();
+var i;
+var li;
+function addItem(){
+    var ul=document.getElementById("ulID");
+    var select=document.getElementById("selID");
+    li=document.createElement("li");
+    var option=document.createElement("option");
+    li.appendChild(document.createTextNode(document.getElementById("inID").value));
+    option.appendChild(document.createTextNode(document.getElementById("inID").value));
+    ul.appendChild(li);
+    select.appendChild(option);
+    a.push(document.getElementById("inID").value);
+    clear();
+}
+function clear(){
+    document.getElementById("inID").value="";
+}
+function addarray(){
+    var ol=document.getElementById("olID");
+    for (i=0; i<a.length; i++ ){
+        li=document.createElement("li");
+        li.appendChild(document.createTextNode(a[i]));
+        ol.appendChild(li);
+    }
+}
